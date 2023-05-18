8f985e3394b50929b3486904b6fa99675bced096
ducphamle2 <ducphamle212@gmail.com>
2023-05-17 22:04:31 -0700

    used oraidex npm package instead of local import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import { OraiswapFactoryClient } from './contracts/OraiswapFactory.client';
-import { InstantiateMsg as Cw20InstantiateMsg } from './contracts/OraiswapToken.types';
+import { OraiswapFactoryClient } from '@oraichain/oraidex-contracts-sdk/build/OraiswapFactory.client';
+import { InstantiateMsg as Cw20InstantiateMsg } from '@oraichain/oraidex-contracts-sdk/build/OraiswapToken.types';
 dotenv.config();
 import { getCosmWasmClient, getSigningStargateClient } from './cosmjs';
 import { TextProposal } from "cosmjs-types/cosmos/gov/v1beta1/gov";
