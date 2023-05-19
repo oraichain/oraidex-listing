70af11a9c8a5536caf6ba4cedf78be2a4f24ca72
Pham Tu <tubackkhoa@gmail.com>
2023-05-19 14:28:29 +0700

    update build

--- packages/contracts-build/src/index.ts
+++ packages/contracts-build/src/index.ts
@@ -16,27 +16,13 @@ export const getContractDir = (name: ContractName = 'oraidex-listing-contract') 
 export const deployContract = async (
   client: SigningCosmWasmClient,
   senderAddress: string,
-  msg: InstantiateMsg,
-  label: string,
-  contractName?: ContractName
+  contractName?: ContractName,
+  msg?: InstantiateMsg,
+  label?: string
 ) => {
   // upload and instantiate the contract
   const wasmBytecode = readFileSync(getContractDir(contractName));
   const uploadRes = await client.upload(senderAddress, wasmBytecode, 'auto');
-  const initRes = await client.instantiate(senderAddress, uploadRes.codeId, msg, label, 'auto');
+  const initRes = await client.instantiate(senderAddress, uploadRes.codeId, msg ?? {}, label ?? contractName, 'auto');
   return { ...uploadRes, ...initRes };
-};
-
-export const migrateContract = async (
-  client: SigningCosmWasmClient,
-  senderAddress: string,
-  contractAddress: string,
-  msg: MigrateMsg,
-  contractName?: ContractName
-) => {
-  // upload and instantiate the contract
-  const wasmBytecode = readFileSync(getContractDir(contractName));
-  const uploadRes = await client.upload(senderAddress, wasmBytecode, 'auto');
-  const migrateRes = await client.migrate(senderAddress, contractAddress, uploadRes.codeId, msg, 'auto');
-  return { ...uploadRes, ...migrateRes };
 };
